17ee3182d5f5b542df97d5a2d0631a5a08b4825e
KizunaG <hecto@LAPTOP-IUQ31NPB>
2025-05-16 13:48:08 -0600

    Creación de clases

---
+++ NominaSystem.Domain/Entities/Empleado.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NominaSystem.Domain.Entities;
+
+public class Empleado
+{
+    public int Id { get; set; }
+    public string Nombre { get; set; }
+    public DateTime? FechaNacimiento { get; set; }
+    public string? Direccion { get; set; }
+    public string? Telefono { get; set; }
+    public string DPI { get; set; }
+    public string EstadoLaboral { get; set; }
+    public DateTime? FechaIngreso { get; set; }
+
+    public int? ID_Cargo { get; set; }
+    public int? ID_Departamento { get; set; }
+}
---
+++ NominaSystem.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using NominaSystem.Domain.Entities;
+
+namespace NominaSystem.Infrastructure.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        // ENTIDADES PRINCIPALES
+        public DbSet<Empleado> Empleados { get; set; }
+        public DbSet<Usuario> Usuarios { get; set; }
+        public DbSet<Rol> Roles { get; set; }
+        public DbSet<Cargo> Cargos { get; set; }
+        public DbSet<Departamento> Departamentos { get; set; }
+
+        // INFORMACIÓN ACADÉMICA Y DOCUMENTOS
+        public DbSet<InformacionAcademica> InformacionAcademica { get; set; }
+        public DbSet<DocumentoEmpleado> DocumentosEmpleado { get; set; }
+        public DbSet<ConfiguracionExpediente> ConfiguracionExpedientes { get; set; }
+        public DbSet<ExpedienteEmpleado> ExpedientesEmpleado { get; set; }
+
+        // NÓMINA Y AJUSTES
+        public DbSet<Nomina> Nominas { get; set; }
+        public DbSet<AjusteNomina> AjustesNomina { get; set; }
+
+        // DESCUENTOS LEGALES
+        public DbSet<DescuentoLegal> DescuentosLegales { get; set; }
+        public DbSet<DetalleDescuentoNomina> DetallesDescuentoNomina { get; set; }
+
+        // AUDITORÍA
+        public DbSet<BitacoraAuditoria> BitacoraAuditoria { get; set; }
+
+        // CONFIGURACIÓN OPCIONAL
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Mapeo adicional si es necesario
+            modelBuilder.Entity<Empleado>()
+                .HasIndex(e => e.DPI)
+                .IsUnique();
+
+            modelBuilder.Entity<Usuario>()
+                .HasIndex(u => u.Usuario)
+                .IsUnique();
+
+            modelBuilder.Entity<ConfiguracionExpediente>()
+                .HasIndex(c => c.TipoDocumento)
+                .IsUnique();
+        }
+    }
+}
+
+
+
