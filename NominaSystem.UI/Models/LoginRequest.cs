0a682856764b80f9c766ce9059a7a29932696e17
BJCorado <brayncorado454@gmail.com>
2025-05-24 09:54:10 -0300

    cambios de login

--- NominaSystem.UI/Models/LoginRequest.cs
+++ NominaSystem.UI/Models/LoginRequest.cs
@@ -2,6 +2,6 @@
 
 public class LoginRequest
 {
-    public string Correo { get; set; } = string.Empty;
+    public string UsuarioNombre { get; set; } = string.Empty;
     public string Contrasena { get; set; } = string.Empty;
 }
--- NominaSystem.UI/Program.cs
+++ NominaSystem.UI/Program.cs
@@ -1,17 +1,38 @@
-using NominaSystem.UI.Services.Interfaces;
+﻿using NominaSystem.UI.Services.Interfaces;
 using NominaSystem.UI.Services;
 using Microsoft.AspNetCore.Components.Server.ProtectedBrowserStorage;
+using Microsoft.AspNetCore.Components.Server;
+using Blazored.LocalStorage;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// Blazor y páginas
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-builder.Services.AddHttpClient();
+
+// HttpClient apuntando al backend correcto (puerto 7122 del WebAPI)
+builder.Services.AddHttpClient<IAuthService, AuthService>(client =>
+{
+    client.BaseAddress = new Uri("https://localhost:7122/");
+});
+
+builder.Services.AddBlazoredLocalStorage();
+
+
+// Servicios inyectables
 builder.Services.AddScoped<ProtectedLocalStorage>();
 
-builder.Services.AddScoped<IAuthService, AuthService>();
+// Componentes interactivos
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+// Mostrar errores de circuito
+builder.Services.Configure<CircuitOptions>(options =>
+{
+    options.DetailedErrors = true;
+});
 
 var app = builder.Build();
-
 
 if (!app.Environment.IsDevelopment())
 {
@@ -20,6 +41,7 @@ if (!app.Environment.IsDevelopment())
 
 app.UseStaticFiles();
 app.UseRouting();
+
 app.MapBlazorHub();
 app.MapFallbackToPage("/_Host");
 
