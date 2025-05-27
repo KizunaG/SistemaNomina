649dad8c8d139857567d1c9944fcc7f086f4c8a4
BJCorado <brayncorado454@gmail.com>
2025-05-26 22:41:09 -0300

    pantalla y logica de expediente empleado, boton para descargar expediente en pdf

---
+++ NominaSystem.UI/Models/InformacionAcademicaDto.cs
@@ -0,0 +1,7 @@
+﻿public class InformacionAcademicaDto
+{
+    public int Id { get; set; }
+    public string Titulo { get; set; } = string.Empty;
+    public string Institucion { get; set; } = string.Empty;
+    public int Anio { get; set; }
+}
--- NominaSystem.UI/Services/Interfaces/IEmpleadoService.cs
+++ NominaSystem.UI/Services/Interfaces/IEmpleadoService.cs
@@ -4,4 +4,7 @@ public interface IEmpleadoService
 {
     Task<List<EmpleadoDto>> ObtenerTodosAsync();
     Task SetAuthTokenAsync(string token);
+
+    Task<bool> ValidarExpediente(int empleadoId);
+
 }
